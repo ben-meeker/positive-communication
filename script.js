44d990c7cab9dd16f8e928ab187e546c48b17196
Ben Meeker <ben@theboyscommittee.com>
2024-07-27 11:55:58 -0600

    added API call to backend

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function printSc() {
-    // your logic
-        let d = 20
-        let sc
-        if (d <= 30) {
-    
-            sc = d + 1
+    fetch("136.36.170.25:3056", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: "What is even that",
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjUzMjIwOTQ2NTZ9.JCuVTr7XjdAoRpy1mO2vsy4Vnl_XLz4veVvMbif-7Wg"
         }
-        return sc
-    }
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+
+        return json
+}
