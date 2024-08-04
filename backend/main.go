2a8d4d930ecbf44aef0f3fa1d77cf673ab0622d7
Ben Meeker <ben@theboyscommittee.com>
2024-08-04 14:57:12 -0600

    new cors method implementation

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,29 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	// a gin router to handle requests
 	router := gin.Default()
+	// use CORS
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://foo.com"},
+		AllowMethods:     []string{"PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	// insert request handlers
-	router.POST("/analyzeMessage", cors, checkAuth, analyzeMessage)
+	router.POST("/analyzeMessage", checkAuth, analyzeMessage)
 	// listen with cert for comdotcomdotcomdotcomdotcomdotcom.com
 	router.RunTLS(":443", "fullchain.pem", "privkey.pem")
 
--- backend/middleware.go
+++ backend/middleware.go
@@ -56,17 +56,3 @@ func checkAuth(c *gin.Context) {
 
 	c.Next()
 }
-
-func cors(c *gin.Context) {
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-
-	if c.Request.Method == "OPTIONS" {
-		c.AbortWithStatus(204)
-		return
-	}
-
-	c.Next()
-}
