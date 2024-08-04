fb2e1bd47f87d15eb545bcc408476f75258f857a
Ben Meeker <ben@theboyscommittee.com>
2024-08-04 14:44:03 -0600

    Fixed duplicate functions

--- backend/handlers.go
+++ backend/handlers.go
@@ -25,9 +25,3 @@ func analyzeMessage(c *gin.Context) {
 
 	c.IndentedJSON(http.StatusOK, response.Choices[0].Message.Content)
 }
-
-func createSentimentAnalysisPrompt(content string) string {
-	parameters := "Please analyze the follow statement, and return it in the following json format. The first field, labelled 'sentiment', that has either a 'positive', 'negative', or 'neutral' value, if it's negative or neutral, a second field labelled 'rephrased_statement', which has a rephrased version of the statement, but with positive language, and a third field labelled 'benefits', which describes the benefits of using the alternate, positive wording. Please make the rephrased statement field an empty string if the sentiment is already positive, and describe the benefits of the given statement in the beneftis section. Make sure to highlight why the rephrased statement is more beneficial than the negative or neutral statement, also be sure to preserve the original intended message. Here is the statement: "
-
-	return parameters + content
-}
