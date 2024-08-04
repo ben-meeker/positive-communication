6f5ad3737761b672f1535fa33eab8607e067fbf6
Ben Meeker <ben@theboyscommittee.com>
2024-08-04 15:25:28 -0600

    custom cors config

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -9,18 +11,17 @@ func main() {
 	// a gin router to handle requests
 	router := gin.Default()
 	// use CORS
-	router.Use(cors.Default())
-	// router.Use(cors.New(cors.Config{
-	// 	AllowOrigins:     []string{"https://ben-meeker.github.io"},
-	// 	AllowMethods:     []string{"GET"},
-	// 	AllowHeaders:     []string{"Origin"},
-	// 	ExposeHeaders:    []string{"Content-Length"},
-	// 	AllowCredentials: true,
-	// 	// AllowOriginFunc: func(origin string) bool {
-	// 	// 	return origin == "https://github.com"
-	// 	// },
-	// 	MaxAge: 12 * time.Hour,
-	// }))
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://ben-meeker.github.io"},
+		AllowMethods:     []string{"POST"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		// AllowOriginFunc: func(origin string) bool {
+		// 	return origin == "https://github.com"
+		// },
+		MaxAge: 12 * time.Hour,
+	}))
 	// insert request handlers
 	router.POST("/analyzeMessage", checkAuth, analyzeMessage)
 	// listen with cert for comdotcomdotcomdotcomdotcomdotcom.com
