2ae8a1ebb7dce4f08dd473030ab027c61a10f544
Ben Meeker <ben@theboyscommittee.com>
2024-08-04 17:55:47 -0600

    changed fetch in printSc()

--- script.js
+++ script.js
@@ -14,9 +14,19 @@ function printSc() {
     body: JSON.stringify(data),
     };
 
-    let response = fetch(apiUrl, requestOptions).then(response=>response.json())
-
-    return response
+    fetch(apiUrl, requestOptions)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        } return response.json();
+    })
+    .then((responseData) => {
+        console.log(responseData);
+    })
+    .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+    });
+    return responseData
 
     // fetch(apiUrl, requestOptions)
     // .then(response => {
